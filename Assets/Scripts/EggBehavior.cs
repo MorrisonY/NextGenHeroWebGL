8098a2fb6d77d1b5392cafbffe09fd6762ef21d3
Morrison Young <35829156+MorrisonY@users.noreply.github.com>
2023-04-16 23:11:48 -0700

    Add files via upload

---
+++ Assets/Scripts/EggBehavior.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class EggBehavior : MonoBehaviour
+{
+    private UserInterface ui;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ui = FindObjectOfType<UserInterface>();
+        ui.EggCountInc();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name.Contains("Wall"))
+        {
+            Destroy(gameObject);
+        }
+
+        if (collision.gameObject.tag.Equals("Enemy"))
+        {
+            Destroy(gameObject);
+            var id = collision.gameObject.GetComponent<PlaneBehavior>().PlaneId;
+            var worldUtils = FindObjectOfType<WorldUtils>();
+            worldUtils.EnemyHit(id);
+            ui.EnemyShot();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        ui.EggCountDec();
+    }
+}
